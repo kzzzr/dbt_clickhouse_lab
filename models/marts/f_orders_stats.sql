ef67ad7944ee9e401f7cef27da732a7f8d9119c8
bailadora <91531507+bailadora@users.noreply.github.com>
2023-08-16 10:30:00 +0300

    Update f_orders_stats.sql

--- models/marts/f_orders_stats.sql
+++ models/marts/f_orders_stats.sql
@@ -12,7 +12,7 @@ SELECT
     , count(DISTINCT O_ORDERKEY) AS num_orders
     , count(DISTINCT C_CUSTKEY) AS num_customers
     , sum(L_EXTENDEDPRICE * L_DISCOUNT) AS revenue
-FROM {{ ref('stg_orders') }}
+FROM {{ ref('f_lineorder_flat') }}
 WHERE 1=1
 GROUP BY
     toYear(O_ORDERDATE)
