9fce4c9f35d6cb3fe18bd78023c56d4fb57d2d2f
AlinaSheik05 <136852582+AlinaSheik05@users.noreply.github.com>
2023-11-11 16:08:20 +0300

    Update f_orders_stats.sql

--- models/marts/f_orders_stats.sql
+++ models/marts/f_orders_stats.sql
@@ -5,4 +5,24 @@
     )
 }}
 
-SELECT 1
+{{
+    config (
+      engine='MergeTree()',
+      order_by=['']
+    )
+}}
+
+SELECT
+    toYear(O_ORDERDATE) AS O_ORDERYEAR
+    , O_ORDERSTATUS
+    , O_ORDERPRIORITY
+    , count(DISTINCT O_ORDERKEY) AS num_orders
+    , count(DISTINCT C_CUSTKEY) AS num_customers
+    , sum(L_EXTENDEDPRICE * L_DISCOUNT) AS revenue
+FROM f_lineorder_flat
+WHERE 1=1
+GROUP BY
+    toYear(O_ORDERDATE)
+    , O_ORDERSTATUS
+    , O_ORDERPRIORITY
+
