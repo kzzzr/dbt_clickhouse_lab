f1b466ff64f1e9104aff0568cf91f90ee6f198be
M1083 <mukhin.038@ya.ru>
2024-01-27 11:40:20 +0000

    modified

--- models/marts/f_orders_stats.sql
+++ models/marts/f_orders_stats.sql
@@ -1,8 +1,22 @@
 {{
     config (
       engine='MergeTree()',
-      order_by=['']
+      order_by=['O_ORDERYEAR', 'O_ORDERSTATUS', 'O_ORDERPRIORITY']
     )
 }}
 
-SELECT 1
+SELECT
+    toYear(O_ORDERDATE) AS O_ORDERYEAR
+    , O_ORDERSTATUS
+    , O_ORDERPRIORITY
+    , count(DISTINCT O_ORDERKEY) AS num_orders
+    , count(DISTINCT C_CUSTKEY) AS num_customers
+    , sum(L_EXTENDEDPRICE * L_DISCOUNT) AS revenue
+FROM {{ ref('stg_lineitem') }} AS l -- PLEASE USE dbt's ref('') to ensure valid DAG execution!
+  INNER JOIN {{ ref('stg_orders') }} AS o ON o.O_ORDERKEY = l.L_ORDERKEY
+  INNER JOIN {{ ref('stg_customer') }} AS c ON c.C_CUSTKEY = o.O_CUSTKEY
+WHERE 1=1
+GROUP BY
+    toYear(O_ORDERDATE)
+    , O_ORDERSTATUS
+    , O_ORDERPRIORITY
